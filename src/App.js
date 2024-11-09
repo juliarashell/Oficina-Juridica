d51c425dd37acfd2ed0d7de3ab2708b58e2e29ea
Julia Rashell <jlopezc121@miumg.edu.gt>
2024-11-08 22:44:12 -0600

    analytics vercel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 import Inicio from './paginas/Inicio';
 import Servicios from './paginas/Servicios';
 import Contacto from './paginas/Contacto';
 import './App.css';
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Route path="/servicios" element={<Servicios />} />
         <Route path="/contacto" element={<Contacto />} />
       </Routes>
+      <Analytics /> {/* Agrega Analytics aquí */}
     </Router>
   );
 }
